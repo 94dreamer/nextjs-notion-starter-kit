9b025a39833c8072adb5df721969e7c3aed9e0ac
94dreamer <503633021@qq.com>
2023-04-11 23:49:06 +0800

    feat: 更改config配置

--- site.config.ts
+++ site.config.ts
@@ -1,45 +1,51 @@
 import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
-  // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  // 站点的根 Notion 页面（必填）
+  rootNotionPageId: '8d8af4e1c6d045d4998db1087e072572',
 
-  // if you want to restrict pages to a single notion workspace (optional)
-  // (this should be a Notion ID; see the docs for how to extract this)
+  // 如果您想将页面限制在单个 notion 工作区（可选）
+  // （这应该是一个 Notion ID；请参阅文档了解如何提取此 ID）
   rootNotionSpaceId: null,
 
-  // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  // 基本站点信息（必填）
+  name: '94梦想家',
+  domain: '94dreamer.com',
+  author: '94dreamer',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  // 开放图谱元数据（可选）
+  
+  description: '94dreamer的个人博客',
 
-  // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  // 社交用户名（可选）
+
+  // twitter: 'transitive_bs',
+  github: '94dreamer',
+  zhihu: '94dreamer',
+  // linkedin: 'fisch2',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
 
-  // default notion icon and cover images for site-wide consistency (optional)
-  // page-specific values will override these site-wide defaults
+
+  // 默认的 notion 图标和封面图片，用于整个站点的一致性（可选）
+  // 页面特定的值将覆盖这些整个站点的默认值
   defaultPageIcon: null,
   defaultPageCover: null,
   defaultPageCoverPosition: 0.5,
 
-  // whether or not to enable support for LQIP preview images (optional)
+  // 是否启用 LQIP 预览图像支持（可选）
   isPreviewImageSupportEnabled: true,
 
-  // whether or not redis is enabled for caching generated preview images (optional)
-  // NOTE: if you enable redis, you need to set the `REDIS_HOST` and `REDIS_PASSWORD`
-  // environment variables. see the readme for more info
+  // 是否启用 Redis 以缓存生成的预览图像（可选）
+  // 注意：如果启用了 Redis，您需要设置 REDIS_HOST 和 REDIS_PASSWORD
+  // 环境变量。有关更多信息，请参阅自述文件
   isRedisEnabled: false,
 
-  // map of notion page IDs to URL paths (optional)
-  // any pages defined here will override their default URL paths
+
+  // notion 页面 ID 到 URL 路径的映射（可选）
+  // 此处定义的任何页面将覆盖其默认 URL 路径
   // example:
   //
   // pageUrlOverrides: {
@@ -48,8 +54,7 @@ export default siteConfig({
   // }
   pageUrlOverrides: null,
 
-  // whether to use the default notion navigation style or a custom one with links to
-  // important pages
+  // 是否使用默认的 notion 导航样式或具有重要页面链接的自定义样式
   navigationStyle: 'default'
   // navigationStyle: 'custom',
   // navigationLinks: [
